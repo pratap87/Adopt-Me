1dafc02985aaa6a4340fdd55dade4e542c102c2a
Arun Pratap Singh Chauhan <apschauhan60@gmail.com>
2020-07-14 11:25:28 +0530

    breed should be declared before usedropdown

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,30 +1,27 @@
-
-import React, { useState } from "react";
-import { ANIMALS } from "@frontendmasters/pet";
+ import React, { useState, useEffect } from "react";
+import pet, { ANIMALS } from "@frontendmasters/pet";
 import useDropdown from "./useDropdown";
 
 const SearchParams = () => {
-    const [location, updateLocation] = useState("Seattle, WA");
-    const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
-    const [breed, BreedDropdown] = useDropdown("Breed", "", breeds);
-const [breeds, updateBreeds] = useState([]);
-
+  const [location, updateLocation] = useState("Seattle, WA");
+  const [breeds, updateBreeds] = useState([]);
+  const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
+  const [breed, BreedDropdown] = useDropdown("Breed", "", breeds);
+ 
   return (
     <div className="search-params">
       <form>
         <label htmlFor="location">
           Location
           <input
-  id="location"
-  value={location}
-  placeholder="Location"
-  onChange={e => updateLocation(e.target.value)}
-/>
-<AnimalDropdown />
-<BreedDropdown />
-
+            id="location"
+            value={location}
+            placeholder="Location"
+            onChange={e => updateLocation(e.target.value)}
+          />
         </label>
- 
+        <AnimalDropdown />
+        <BreedDropdown />
         <button>Submit</button>
       </form>
     </div>
